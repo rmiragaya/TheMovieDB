b15cd5433ef5326b41384f639e556d24122e2a04
RodrigoMiragaya <rodrigo.e.miragaya@gmail.com>
2022-12-22 11:57:43 -0300

    add viewmodel tests

---
+++ app/src/test/java/com/tapdeveloper/themoviedb/presentation/movielist/MoviesViewmodelTest.kt
@@ -0,0 +1,115 @@
+package com.tapdeveloper.themoviedb.presentation.movielist
+
+import com.google.common.truth.Truth.assertThat
+import com.tapdeveloper.themoviedb.presentation.moviesList.MoviesViewmodel
+import com.tapdeveloper.themoviedb.utils.FakeMovieRepo
+import com.tapdeveloper.themoviedb.utils.MainDispatcherRule
+import com.tapdeveloper.themoviedb.utils.MoviesListMock
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MoviesViewmodelTest {
+
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    private lateinit var viewModel: MoviesViewmodel
+
+    @Before
+    fun setUp() {
+        viewModel = MoviesViewmodel(FakeMovieRepo())
+    }
+
+    @Test
+    fun whenLoadNextMovies_ResponseUpdate() {
+        runTest {
+            assertThat(viewModel.moviesResponse.movies).isEmpty()
+            viewModel.loadNextMovies()
+            advanceUntilIdle()
+            assertThat(viewModel.moviesResponse.movies).containsExactlyElementsIn(MoviesListMock.getMovies())
+        }
+    }
+
+    @Test
+    fun shouldntFetchMoreWhenScrollOnTop() {
+        runTest {
+            viewModel.loadNextMovies()
+            advanceUntilIdle()
+            assertThat(viewModel.shouldFetchMoreMovies(0)).isFalse()
+        }
+    }
+
+    @Test
+    fun shouldFetchMoreWhenScrollOnBottom() {
+        runTest {
+            viewModel.loadNextMovies()
+            advanceUntilIdle()
+            assertThat(viewModel.shouldFetchMoreMovies(MoviesListMock.getListSize())).isTrue()
+        }
+    }
+
+    @Test
+    fun searchQueryShowResults() {
+        runTest {
+            val query = "title1"
+            viewModel.loadNextMovies()
+            advanceUntilIdle()
+            assertThat(viewModel.moviesResponse.movies).containsExactlyElementsIn(MoviesListMock.getMovies())
+            viewModel.searchMovies(query)
+            advanceUntilIdle()
+            assertThat(viewModel.moviesResponse.movies).containsExactlyElementsIn(
+                MoviesListMock.getMovies().filter { it.title!!.contains(query) })
+        }
+    }
+
+    @Test
+    fun whenSelectingAMovie_ThenTheMovieIsSelected(){
+        val movie = MoviesListMock.getMovies()[0]
+        viewModel.selectMovie(movie)
+        assertThat(viewModel.selectedMovie).isEqualTo(movie)
+    }
+
+    @Test
+    fun listEmptyWhenNoFavoritesMovies() {
+        runTest {
+            viewModel.getFavoritesMovies()
+            advanceUntilIdle()
+            assertThat(viewModel.favoritesMovies).isEmpty()
+        }
+    }
+
+    @Test
+    fun favoriteMovieShouldBeAddedToTheList() {
+        runTest {
+            assertThat(viewModel.favoritesMovies).isEmpty()
+            val movie = MoviesListMock.getMovies().first()
+            viewModel.selectMovie(movie)
+            viewModel.addOrRemoveFavorites()
+            viewModel.getFavoritesMovies()
+            advanceUntilIdle()
+            assertThat(viewModel.favoritesMovies).contains(movie)
+        }
+    }
+
+    @Test
+    fun removedFavoriteMovieShouldBeRemovedFromTheList() {
+        runTest {
+            assertThat(viewModel.favoritesMovies).isEmpty()
+            val movie = MoviesListMock.getMovies().first()
+            viewModel.selectMovie(movie)
+            viewModel.addOrRemoveFavorites()
+            viewModel.getFavoritesMovies()
+            advanceUntilIdle()
+            assertThat(viewModel.favoritesMovies).contains(movie)
+
+            viewModel.addOrRemoveFavorites()
+            advanceUntilIdle()
+            assertThat(viewModel.favoritesMovies).isEmpty()
+        }
+    }
+}
---
+++ app/src/test/java/com/tapdeveloper/themoviedb/utils/FakeMovieRepo.kt
@@ -0,0 +1,35 @@
+package com.tapdeveloper.themoviedb.utils
+
+import com.tapdeveloper.themoviedb.domain.model.Movie
+import com.tapdeveloper.themoviedb.domain.model.MoviesListResponse
+import com.tapdeveloper.themoviedb.domain.repository.MovieRepository
+import com.tapdeveloper.themoviedb.util.Resource
+
+class FakeMovieRepo : MovieRepository {
+
+    private val subscribeMovies = mutableListOf<Movie>()
+    private val movies = MoviesListMock.getMovies()
+
+    override suspend fun getMovies(page: Int?): Resource<MoviesListResponse> {
+       return Resource.Success(data = MoviesListResponse(movies = movies))
+    }
+
+    override suspend fun searchMovies(query: String, page: Int?): Resource<MoviesListResponse> {
+       return Resource.Success(data = MoviesListResponse(movies = movies.filter { it.title!!.contains(query) }))
+    }
+
+    override suspend fun isMovieFavorites(movie: Movie): Resource<Boolean> {
+        return Resource.Success(data = subscribeMovies.contains(movie))
+    }
+
+    override suspend fun getFavoritesMovies(): Resource<List<Movie>> {
+        return Resource.Success(data = subscribeMovies)
+    }
+
+    override suspend fun addMovieToFavorites(movie: Movie): Boolean =
+        subscribeMovies.add(movie)
+
+    override suspend fun removeMovieFromFavorites(movie: Movie) {
+        subscribeMovies.remove(movie)
+    }
+}

3c88fcb4ad5d1830454e3a3468cf1b58e7e6fd35
RodrigoMiragaya <rodrigo.e.miragaya@gmail.com>
2022-12-23 13:59:26 -0300

    Add splash screen

--- app/src/main/java/com/tapdeveloper/themoviedb/MainActivity.kt
+++ app/src/main/java/com/tapdeveloper/themoviedb/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.tapdeveloper.themoviedb.presentation.moviesList.MoviesViewmodel
 import com.tapdeveloper.themoviedb.presentation.navigation.Navigation
@@ -17,8 +18,16 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         viewModel.loadNextMovies()
         viewModel.getFavoritesMovies()
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.moviesResponse.movies.isEmpty()
+            }
+        }
+
         setContent {
             TheMovieDBTheme {
                 Navigation(navController = rememberNavController())

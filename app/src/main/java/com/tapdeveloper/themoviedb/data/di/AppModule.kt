a0565898cacf1d50d1f4efd410cf8f3feb3dac31
Rmiragaya <39171888+rmiragaya@users.noreply.github.com>
2023-01-12 11:59:18 -0300

    add launcher icon, remove oldone (#2)
    * add launcher icon, remove oldone
    
    * remove .idea folder from git

--- app/src/main/java/com/tapdeveloper/themoviedb/data/di/AppModule.kt
+++ app/src/main/java/com/tapdeveloper/themoviedb/data/di/AppModule.kt
@@ -14,6 +14,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
@@ -37,7 +38,9 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun providesVendorDatabase(app: Application): MovieDatabase {
+    fun providesVendorDatabase(
+        app: Application
+    ): MovieDatabase {
         return Room.databaseBuilder(
             app,
             MovieDatabase::class.java,
